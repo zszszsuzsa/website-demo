b6e66d25e566fd26df546030d332572f87dcaed0
Zsuzsanna Szöllősi <szollosi.zsuzsa1@gmail.com>
2018-04-18 02:10:43 +0200

    init

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.video').hover(function toggleControls() {
+    if (this.hasAttribute("controls")) {
+        this.removeAttribute("controls")
+    } else {
+        this.setAttribute("controls", "controls")
+    }
+})
